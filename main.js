d1dd895fb812fc26fa8bdff73f08556cc8beb59f
Pavlo Reshetylo <pavlo_reshetylo@epam.com>
2021-07-23 10:08:45 +0300

    fix footer position. fix cards width

--- main.js
+++ main.js
@@ -135,7 +135,7 @@ function clearScope() {
     selectedCard = 0;
     results = [0, 0, 0];
 }
-let ver = "1.6.3";
+let ver = "1.6.4";
 
 document.getElementById("version").innerText = "Version: " + ver;
 clearScope()
