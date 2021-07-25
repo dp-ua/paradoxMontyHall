093ad2a3819f920b58f62a05ba06a23b2aea3af6
Pavlo Reshetylo <pavlo_reshetylo@epam.com>
2021-07-25 15:21:11 +0300

    add languages

--- main.js
+++ main.js
@@ -1,8 +1,126 @@
-const round1Text = "Раунд 1.<br/>Выберите карту";
-const round2Text = "Раунд 2. <br/> Я открыл одну из двух оставшихся карт.<br/> Выберите карту";
-const newGameText = " Для начала новой игры - нажмите на любую карту. ";
-const winnerText = "Вы выиграли :)";
-const looserText = "Вы проиграли :(";
+function getLang() {
+    var url = location.href;
+    let lang = url.match(/lang=([^&]+)/);
+    return lang == null ? "ru" : lang[1].toLocaleLowerCase();
+}
+
+let language = getLang();
+
+function round1Text() {
+    switch (language) {
+        case "ua":
+            return "Раунд 1.<br/>Оберіть карту";
+        case "en":
+            return "Round 1.<br/>Choose the card";
+        default:
+            return "Раунд 1.<br/>Выберите карту";
+    }
+}
+
+function round2Text() {
+    switch (language) {
+        case "ua":
+            return "Раунд 2.<br/>Я відкрив іншу карту<br/> Оберіть карту";
+        case "en":
+            return "Round 2.<br/>I have opened another card.<br/> Choose the card";
+        default:
+            return "Раунд 2. <br/> Я открыл одну из двух оставшихся карт.<br/> Выберите карту";
+    }
+}
+
+
+function winnerText() {
+    switch (language) {
+        case "ua":
+            return "Ві виграли :)";
+        case "en":
+            return "You won :)";
+        default:
+            return "Вы выиграли :)";
+    }
+}
+
+function looserText() {
+    switch (language) {
+        case "ua":
+            return "Ви програли :(";
+        case "en":
+            return "You lost :(";
+        default:
+            return "Вы проиграли :(";
+    }
+}
+
+function newGame() {
+    switch (language) {
+        case "ua":
+            return "Натисніть на будь-яку карту для продовження..."
+        case "en":
+            return "Press any card to continue...";
+        default:
+            return "Нажмите на любую карту для продолжения...";
+    }
+}
+
+function rulesTitle() {
+    switch (language) {
+        case "en":
+            return "Rules";
+        default:
+            return "Правила";
+    }
+}
+
+function rule() {
+    switch (language) {
+        case "ua":
+            return "<ul> <li> Ві граєте у вікторину. </li> <li> Вам пропонуються три карти. За одною с них ховається приз. </li> <li> У першому раунді ви можете обрати будь-яку з трьох карт. </li> <li> Після цього я відкрию одну з двох, що залишилися і не має призу. </li> <Li> У другому раунді ви маєте знов зробити вибір. Ви можете затвердити його або обрати іншу карту. </li> <li>Хай щастить :) </li> </ ul>";
+        case "en":
+            return "<ul> <li> You play a quiz. </li> <li> Three cards are offered for your. One of them hides a prize. </li> <li> In the first round you can choose any of them. </li> <li> After that I will open one of the two free cards without prize. </li> <Li> In the second round, you can confirm(select the same card like at first time) or change your choice(choose another card). </li> <li> Good Luck :) </li> </ ul>";
+        default:
+            return "<ul> <li> Вы играете в викторину. </li> <li> Вашему вниманию предлагаются три карты. За одной из них находится приз. </li> <li> В первом раунде вы можете выбрать любую из трех карт. </li> <li> После этого я открою одну из двух оставшихся, где приза нет. </li> <li> Во втором раунде вы можете подтвердить свой выбор или изменить решение и выбрать другую карту. </li> <li> Удачи: ) </li> </ul>";
+    }
+}
+
+const ru = document.getElementById("ru");
+const ua = document.getElementById("ua");
+const en = document.getElementById("en");
+let langs = Array(ru, ua, en);
+
+ru.addEventListener('click', () => chooseLang(ru));
+ua.addEventListener('click', () => chooseLang(ua));
+en.addEventListener('click', () => chooseLang(en));
+
+function chooseLang(lang) {
+    clearLang();
+    lang.classList.add("selectLang");
+    language = lang.innerText.toLowerCase();
+    changeLanguage();
+}
+
+
+function changeLanguage() {
+    switch (round) {
+        case 1:
+            title.innerHTML = round1Text();
+            break;
+        case 2:
+            title.innerHTML = round2Text();
+            break;
+        default:
+            title.innerHTML = "";
+    }
+    document.getElementById("rule").innerHTML = rule();
+    document.getElementById("rules-title").innerText = rulesTitle();
+    document.getElementById("newGame").innerText = newGame();
+}
+
+function clearLang() {
+    langs.forEach(lang => {
+        lang.classList.remove("selectLang");
+    });
+}
+
 
 const card1 = document.getElementById("1");
 const card2 = document.getElementById("2");
@@ -41,9 +159,11 @@ function unselectCards() {
     });
 }
 
+
+
 function startGame() {
     document.getElementById("newGame").classList.add("hide");
-    title.innerHTML = round1Text;
+    title.innerHTML = round1Text();
     round = 1;
     randomCard = getRandom(0, 2);
     changeResults();
@@ -82,7 +202,7 @@ function arrayRandElement(arr) {
 function selectCard() {
     switch (round) {
         case 1:
-            title.innerHTML = round2Text;
+            title.innerHTML = round2Text();
             round = 2;
             let freeCards = [];
             cards.forEach(function(card, index, cards) {
@@ -93,10 +213,10 @@ function selectCard() {
         case 2:
             document.getElementById("newGame").classList.remove("hide");
             if (selectedCard == randomCard) {
-                title.innerHTML = winnerText;
+                title.innerHTML = winnerText();
                 good++;
             } else {
-                title.innerHTML = looserText;
+                title.innerHTML = looserText();
                 bad++;
             }
             clearCards();
@@ -134,7 +254,7 @@ function clearScope() {
     selectedCard = 0;
     results = [0, 0, 0];
 }
-let ver = "1.6.6";
+let ver = "2.0.1";
 
 /*
     test just random choosen card. 
@@ -166,6 +286,7 @@ function testChange() {
 document.getElementById("version").innerText = "Version: " + ver;
 clearScope()
 startGame();
+chooseLang(document.getElementById(language));
 
 
 /* remove comment if you whant to run tests
